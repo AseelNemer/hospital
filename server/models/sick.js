9545b514c3e4803c3ddf460032cb582e3e0f044c
shatha <shathab@ac.sce.ac.il>
2022-09-29 16:22:26 +0300

    build the server

---
+++ server/models/sick.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+
+
+//const configuration = require('../configuration')
+
+// let sickSchema = new mongoose.Schema({
+//     number_id: { type: String, default: "" },
+//     gender: {
+// 		// true = male
+// 		// false = female
+// 		type: Boolean,
+// 		default: true
+// 	},
+//     first_name: { type: String, default: "" },
+//     last_name: { type: String, default: "" },
+//     age: { type: Number, default: null },
+//     city: { type: String, default: "" },
+//     mail: { type: String, default: "" },
+//     complaint: [complaintSchema], // תלונות
+//     recommendation: [recommendationSchema], // המלצות
+//     Medication: [MedicationSchema], // המלצות
+
+// })
+
+const doctorSchema=mongoose.Schema({
+
+})
